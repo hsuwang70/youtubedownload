f67ab2b7f20b7ae61aa2dd1cb3be213f128170bb
hsuwang70 <hsuwang70@gmail.com>
2024-08-12 11:56:58 +0800

    first commit

---
+++ api/app.py
@@ -0,0 +1,45 @@
+import yt_dlp
+from flask import Flask, request, Response
+from flask_cors import CORS
+import json
+import os
+
+app = Flask(__name__)
+CORS(app)
+request_methods = ["POST"]
+
+def get_download_folder():
+    # 獲取使用者的 Downloads 資料夾路徑
+    if os.name == 'nt':  # Windows
+        return os.path.join(os.environ['USERPROFILE'], 'Downloads')
+    else:  # macOS 或 Linux
+        return os.path.join(os.environ['HOME'], 'Downloads')
+
+def dowload_youtube(request_data):
+
+    url = request_data['url']
+    download_folder = get_download_folder()
+    print(download_folder)
+
+    output_template = os.path.join(download_folder, 'downloaded_video.%(ext)s')
+
+    ydl_opts = {
+        'outtmpl': output_template,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+@app.route('/dowloadyoutube', methods=['POST'])
+def getreport():
+    try:
+        request_data = request.json
+        result = dowload_youtube(request_data)
+        # request_data = request.json
+        return Response(json.dumps(result), mimetype="application/json")
+    except Exception as e:
+        return Response(json.dumps({"status": 0, "error_msg": str(e)}), mimetype='application/json'), 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=3000,debug=True)
